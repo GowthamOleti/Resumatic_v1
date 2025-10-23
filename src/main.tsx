eb94cb67a2c1bbe9dc58b2970051b3b5f53c8996
Gowtham Oleti <gowtham.oleti@zomato.com>
2025-10-23 23:37:25 +0530

    Remove dark mode completely from the application

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { ThemeProvider } from './contexts/ThemeContext'
 import HomePage from './pages/HomePage'
 import BuilderPage from './pages/BuilderPage'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/builder" element={<BuilderPage />} />
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/builder" element={<BuilderPage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
